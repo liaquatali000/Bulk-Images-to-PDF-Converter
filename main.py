7e2e6cf4c328dc76fa56c449f841bd082fcfa8d6
liaquatali000 <49384878+liaquatali000@users.noreply.github.com>
2025-02-24 10:15:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 /*
- * This file is part of Image & PDF Converter.
+ * This file is part of Images and PDF Converter.
  *
  * Image & PDF Converter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
